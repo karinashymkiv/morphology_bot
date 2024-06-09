c305bb52f78c5f977c57f7c797b3702841e7cee1
karinashymkiv <93536952+karinashymkiv@users.noreply.github.com>
2024-06-09 16:36:54 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{fs::File, sync::Arc};
 use chatgpt::{client::ChatGPT, config::ChatGPTEngine};
 use dotenv::dotenv;
 use log::debug;
-use quiz::{ai_helper::QuizHelper, parts::PartsSentences};
+use quiz::{ai_helper::QuizHelper, declension::{self, Declension}, parts::PartsSentences};
 use teloxide::{
     dispatching::dialogue::{serializer::Json, ErasedStorage, SqliteStorage, Storage},
     prelude::*,
@@ -33,6 +33,12 @@ pub enum State {
         question_number: usize,
         score: usize,
     },
+    DeclensionsRecieveAmountOfQuestions,
+    DeclensionsQuiz {
+        quiz: quiz::Quiz,
+        question_number: usize,
+        score: usize,
+    },
 }
 
 type UserInfoStorage = std::sync::Arc<ErasedStorage<State>>;
@@ -43,33 +49,39 @@ async fn main() {
     let CHATGPT_API_KEY = std::env::var("CHATGPT_API_KEY").expect("CHATGPT_API_KEY is not set");
 
     pretty_env_logger::init();
-    log::info!("Starting dialogue bot...");
 
     let bot = Bot::from_env();
 
-    println!("Establishing connection to the database...");
+    print!("Establishing connection to the database... ");
     let storage: UserInfoStorage = SqliteStorage::open("db.sqlite", Json)
         .await
         .unwrap()
         .erase();
-    println!("Connection established");
+    println!("ESTEBLISHED");
 
     // Load the dictionary of stressed words
-    println!("Loading the dictionary of stressed words");
+    print!("Loading the dictionary of stressed words... ");
     let stressed_words_dictionary = Arc::new(quiz::stress::StressWords::new(
         File::open("stress.txt").expect("Failed to open file 'stress.txt'"),
     ));
-    println!("Dictionary loaded");
+    println!("LOADED");
 
     // Load the conllu file w/ the Ukrainian treebank
     // TODO? Implement a way to use the Ukrainian treebank to generate questions
 
-    println!("Loading the conllu file");
+    print!("Loading the conllu file... ");
     let conllu_file = File::open("uk_iu-ud-dev.conllu").expect("Failed to open conllu file");
     let conllu_doc = PartsSentences::new(conllu_file);
     let conllu_doc = Arc::new(conllu_doc);
+    println!("LOADED");
 
-    println!("Conllu file loaded");
+    print!("Loading the declension dictionary... ");
+    let declension_file = File::open("words_with_declensions.json").expect("Failed to open declension file");
+    let declension_file = quiz::declension::Declension::new(declension_file);
+    let declension_file = Arc::new(declension_file);
+    println!("LOADED");
+
+    print!("Creating the ChatGPT instance... ");
 
     let gpt = {
         let mut gpt = ChatGPT::new(CHATGPT_API_KEY).expect("Unable to connect with ChatGPT");
@@ -85,6 +97,8 @@ async fn main() {
         quiz::ai_helper::Personality::Shevchenko,
     ));
     let quiz_helper_for_parts = quiz_helper.clone();
+    let quiz_helper_for_declensions = quiz_helper.clone();
+    println!("CREATED");
 
     Dispatcher::builder(
         bot,
@@ -158,6 +172,39 @@ async fn main() {
                         )
                     },
                 ),
+            )
+            .branch(
+                dptree::case![State::DeclensionsRecieveAmountOfQuestions].endpoint(
+                    move |bot: Bot, dialogue: QuizDialogue, msg: Message| {
+                        receive_amount_of_questions_declensions(
+                            declension_file.clone(),
+                            bot,
+                            dialogue,
+                            msg,
+                        )
+                    },
+                ),
+            )
+            .branch(
+                dptree::case![State::DeclensionsQuiz {
+                    quiz,
+                    question_number,
+                    score
+                }]
+                .endpoint(
+                    move |bot: Bot,
+                          dialogue: QuizDialogue,
+                          (quiz, question_number, score): (quiz::Quiz, usize, usize),
+                          msg: Message| {
+                        declensions_quiz(
+                            quiz_helper_for_declensions.clone(),
+                            bot,
+                            dialogue,
+                            (quiz.clone(), question_number, score),
+                            msg,
+                        )
+                    },
+                )
             ),
     )
     .dependencies(dptree::deps![storage])
@@ -177,6 +224,7 @@ async fn start(bot: Bot, dialogue: QuizDialogue, msg: Message) -> HandlerResult 
 
 const STRESSED_WORDS_GAME: &str = "Почати тест на наголос";
 const PARTS_OF_SPEECH_GAME: &str = "Почати тест на частини мови";
+const DECLENSION_GAME: &str = "Почати тест на відмінювання";
 async fn receive_full_name(bot: Bot, dialogue: QuizDialogue, msg: Message) -> HandlerResult {
     match msg.text() {
         Some(full_name) => {
@@ -196,7 +244,8 @@ async fn receive_full_name(bot: Bot, dialogue: QuizDialogue, msg: Message) -> Ha
     let keyboard = KeyboardMarkup::new(vec![vec![
         KeyboardButton::new(STRESSED_WORDS_GAME),
         KeyboardButton::new(PARTS_OF_SPEECH_GAME),
-    ]]);
+    ], vec![KeyboardButton::new(DECLENSION_GAME),]]);
+
     bot.send_message(msg.chat.id, "Що б ти хотів зробити?")
         .reply_markup(keyboard)
         .await?;
@@ -233,6 +282,20 @@ async fn receive_game_choice(bot: Bot, dialogue: QuizDialogue, msg: Message) -> 
                 .await?;
             dialogue
                 .update(State::PartsOfSpeechRecieveAmountOfQuestions)
+                .await?;
+            return Ok(());
+        }
+        Some(DECLENSION_GAME) => {
+            let keyboard = KeyboardMarkup::new(vec![
+                vec![KeyboardButton::new("5")],
+                vec![KeyboardButton::new("10")],
+                vec![KeyboardButton::new("15")],
+            ]);
+            bot.send_message(msg.chat.id, "Обери кількість питань")
+                .reply_markup(keyboard)
+                .await?;
+            dialogue
+                .update(State::DeclensionsRecieveAmountOfQuestions)
                 .await?;
             return Ok(());
         }
@@ -319,7 +382,7 @@ async fn stressed_quiz(
         let keyboard = KeyboardMarkup::new(vec![vec![
             KeyboardButton::new(STRESSED_WORDS_GAME),
             KeyboardButton::new(PARTS_OF_SPEECH_GAME),
-        ]]);
+        ], vec![KeyboardButton::new(DECLENSION_GAME),]]);
         let quiz_score = format!(
             "Квіз закінчився! Ти відповів правильно на {} з {} питань\nЩо б ти хотів зробити далі?",
             current_score,
@@ -375,7 +438,7 @@ async fn stressed_quiz(
 }
 
 async fn receive_amount_of_questions_parts_of_speech(
-    colllu_doc: Arc<PartsSentences>,
+    collu_doc: Arc<PartsSentences>,
     bot: Bot,
     dialogue: QuizDialogue,
     msg: Message,
@@ -402,7 +465,7 @@ async fn receive_amount_of_questions_parts_of_speech(
     let quiz = quiz::Quiz::new(
         (0..amount)
             .map(|_| {
-                let random_sentence = colllu_doc.get_random_sentence();
+                let random_sentence = collu_doc.get_random_sentence();
                 random_sentence.generate_question()
             })
             .collect(),
@@ -444,7 +507,7 @@ async fn parts_of_speech_quiz(
                 .await;
 
             let ai_reply: String = ai_helper
-                .generate_reply_to_wrong_parts_answer(question.clone())
+                .generate_reply_to_wrong_parts_answer(question.clone(), answer.to_string())
                 // If the AI fails to generate a reply, we'll just tell the user the correct answer
                 // Sometimes it may happen due to timeout or other reasons
                 .await.unwrap_or(format!("Правильна відповідь -- {} Будь уважнішим!", correct_answer.text));
@@ -457,7 +520,7 @@ async fn parts_of_speech_quiz(
         let keyboard = KeyboardMarkup::new(vec![vec![
             KeyboardButton::new(STRESSED_WORDS_GAME),
             KeyboardButton::new(PARTS_OF_SPEECH_GAME),
-        ]]);
+        ], vec![KeyboardButton::new(DECLENSION_GAME),]]);
         let quiz_score = format!(
             "Квіз закінчився! Ти відповів правильно на {} з {} питань\nЩо б ти хотів зробити далі?",
             current_score,
@@ -493,6 +556,132 @@ async fn parts_of_speech_quiz(
             quiz,
             question_number: question_number + 1,
             score: current_score,
+        })
+        .await?;
+    Ok(())
+}
+
+async fn declensions_quiz(
+    ai_helper: Arc<QuizHelper>,
+    bot: Bot,
+    dialogue: QuizDialogue,
+    (quiz, question_number, score): (quiz::Quiz, usize, usize),
+    msg: Message,
+) -> HandlerResult {
+    let mut current_score = score;
+    if question_number != 0 {
+        let answer = msg.text().unwrap();
+        let question = &quiz.questions[question_number - 1];
+        let correct_answer = question.answers.iter().find(|a| a.is_correct).unwrap();
+        if answer == correct_answer.text {
+            bot.send_message(msg.chat.id, "Правильно!").await?;
+            current_score += 1;
+        } else {
+            // We don't really care about the result here, so we'll just ignore the error if this action is unsuccessful
+            // But it adds to the user's experience if it works!
+            let _ = bot.send_chat_action(msg.chat.id, ChatAction::Typing)
+                .await;
+
+            let ai_reply: String = ai_helper
+                .generate_reply_to_wrong_parts_answer(question.clone(), answer.to_string())
+                // If the AI fails to generate a reply, we'll just tell the user the correct answer
+                // Sometimes it may happen due to timeout or other reasons
+                .await.unwrap_or(format!("Правильна відповідь -- {} Будь уважнішим!", correct_answer.text));
+
+            bot.send_message(msg.chat.id, format!("Неправильно!\n\n{}", ai_reply)).await?;
+        }
+    }
+
+    if question_number >= quiz.questions.len() {
+        let keyboard = KeyboardMarkup::new(vec![vec![
+            KeyboardButton::new(STRESSED_WORDS_GAME),
+            KeyboardButton::new(PARTS_OF_SPEECH_GAME),
+        ], vec![KeyboardButton::new(DECLENSION_GAME),]]);
+
+        let quiz_score = format!(
+            "Квіз закінчився! Ти відповів правильно на {} з {} питань\nЩо б ти хотів зробити далі?",
+            current_score,
+            quiz.questions.len()
+        );
+        bot.send_message(msg.chat.id, quiz_score.as_str())
+            .reply_markup(keyboard)
+            .await?;
+
+        dialogue.update(State::RecieveGameChoice).await?;
+        return Ok(());
+    }
+
+    let question = &quiz.questions[question_number];
+    let answers = &question
+        .answers
+        .iter()
+        .map(|a| a.text.clone())
+        .collect::<Vec<_>>();
+
+    bot.send_message(msg.chat.id, question.clone().text)
+        .parse_mode(ParseMode::Html)
+        .reply_markup(KeyboardMarkup::new(
+            answers
+                .iter()
+                .map(|a| vec![KeyboardButton::new(a.clone())])
+                .collect::<Vec<_>>(),
+        ))
+        .await?;
+
+    dialogue
+        .update(State::PartsOfSpeechQuiz {
+            quiz,
+            question_number: question_number + 1,
+            score: current_score,
+        })
+        .await?;
+    Ok(())
+}
+
+async fn receive_amount_of_questions_declensions(
+    words: Arc<Declension>,
+    bot: Bot,
+    dialogue: QuizDialogue,
+    msg: Message,
+) -> HandlerResult {
+    if let None = msg.text() {
+        bot.send_message(msg.chat.id, "Будь ласка, введіть число")
+            .await?;
+        return Ok(());
+    }
+    if let Err(_) = msg.text().unwrap().parse::<usize>() {
+        bot.send_message(msg.chat.id, "Будь ласка, введіть число")
+            .await?;
+        return Ok(());
+    }
+
+    // It is safe to unwrap here because we've already checked that the input is a number
+    let amount: usize = msg.text().unwrap().parse().unwrap();
+    if amount == 0 {
+        bot.send_message(msg.chat.id, "Кількість питань не може бути 0")
+            .await?;
+        return Ok(());
+    }
+
+    let mut questions: Vec<quiz::Question> = Vec::new();
+    while questions.len() < amount {
+        let random_noun = words.get_random_noun();
+        if let Ok(q) = random_noun.generate_question_out_of_noun() {
+            questions.push(q);
+        }
+    }
+
+    let quiz = quiz::Quiz::new(questions);
+
+    bot.send_message(msg.chat.id, "Чудово! Почнемо тест!")
+        .reply_markup(KeyboardMarkup::new(vec![vec![KeyboardButton::new("Вйо!")]]))
+        .await?;
+
+    dialogue
+        .update(State::DeclensionsQuiz {
+            quiz,
+            question_number: 0,
+            score: 0,
         })
         .await?;
     Ok(())
